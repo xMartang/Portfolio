a18fd8bd937e5bc8e49c7f1e0bc8543870aa13b7
Matan Afriat <matanafriat@gmail.com>
2024-03-11 16:31:21 +0200

    Created Header Component

---
+++ frontend/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Header from "@/app/components/_Header/Header"
+
+export default function Index() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <Header/>
+    </main>
+  );
+}
